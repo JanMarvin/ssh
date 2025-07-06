da6ad493238acb4aa5e97b84679cd0493537b1ce
Jan Marvin Garbuszus <jan.garbuszus@ruhr-uni-bochum.de>
2025-07-06 17:22:42 +0200

    use winlibs.R

--- tools/winlibs.R
+++ tools/winlibs.R
@@ -1,19 +1,18 @@
-if(!file.exists('session.o') && !file.exists("../.deps/protobuf/include/libssh/libssh.h")){
-  unlink("../.deps", recursive = TRUE)
+if(!file.exists("../windows/libssl/include/openssl/pem.h")){
+  unlink("../windows", recursive = TRUE)
   url <- if(grepl("aarch", R.version$platform)){
-    "https://github.com/r-windows/bundles/releases/download/libssh-0.11.0/libssh-0.11.0-clang-aarch64.tar.xz"
+    "https://github.com/r-windows/bundles/releases/download/openssl-3.4.0/openssl-3.4.0-clang-aarch64.tar.xz"
   } else if(grepl("clang", Sys.getenv('R_COMPILED_BY'))){
-    "https://github.com/r-windows/bundles/releases/download/libssh-0.11.0/libssh-0.11.0-clang-x86_64.tar.xz"
-  } else if(getRversion() >= "4.2") {
-    "https://github.com/r-windows/bundles/releases/download/libssh-0.11.0/libssh-0.11.0-ucrt-x86_64.tar.xz"
+    "https://github.com/r-windows/bundles/releases/download/openssl-3.4.0/openssl-3.4.0-clang-x86_64.tar.xz"
+  }  else if(getRversion() >= "4.2") {
+    "https://github.com/r-windows/bundles/releases/download/openssl-3.4.0/openssl-3.4.0-ucrt-x86_64.tar.xz"
   } else {
-    "https://github.com/rwinlib/libssh/archive/v0.10.5.tar.gz"
+    "https://github.com/rwinlib/openssl/archive/v3.1.1.tar.gz"
   }
   download.file(url, basename(url), quiet = TRUE)
-  dir.create("../.deps", showWarnings = FALSE)
-  untar(basename(url), exdir = "../.deps", tar = 'internal')
+  dir.create("../windows", showWarnings = FALSE)
+  untar(basename(url), exdir = "../windows", tar = 'internal')
   unlink(basename(url))
-  setwd("../.deps")
-  file.rename(list.files(), 'libssh')
-  invisible()
+  setwd("../windows")
+  file.rename(list.files(), 'libssl')
 }
